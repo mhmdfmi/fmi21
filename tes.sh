2d7ae87b916ab21ff3d2215390a0977977607b44
mhmdfmi <83300945+mhmdfmi@users.noreply.github.com>
2021-09-23 23:28:56 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,42 @@
+clear 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo 
+toilet -f big -F gay by Fahmi  
+echo $green"selamat Datang"
+echo 
+sleep 2
+echo $green"silahkan pilih tools nya :"
+echo 
+echo $cyan"1.) stabilkan jaringan"
+echo 
+echo $blue"0.) keluar aja !!!"
+echo 
+echo $white 
+read -p "pilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then 
+clear
+toilet -f big -F gay Fahmi
+echo $green"stabilkan jaringan by Fahmi"
+sleep 2
+ping -s 1000 1.1.1.1
+fi 
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then 
+clear 
+echo $yellow
+figlet "Bye"
+echo $white"To Be Continued"
+sleep 3
+exit 
+fi
+
